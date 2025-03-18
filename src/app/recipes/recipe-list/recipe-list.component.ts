5bb7ee645527378f2e9d82e3ba7a9c4b5b2658ad
baiching <bachmarma@gmail.com>
2025-03-18 13:40:06 +0100

    added new recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class RecipeListComponent {
 
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'A test Recipe description', 'https://realfood.tesco.com/media/images/1400x919-LENTIL-AND-AUBERGINE-HOT-POT-0190-56-9cda05b2-2bb5-4cc3-a749-deb37927ca10-0-1400x919.jpg'),
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {}
 }
